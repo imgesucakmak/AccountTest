3040005d4803b6b0bfa5b2c18ba75ed434d18614
imgesucakmak <cakmakimgesu@gmail.com>
2024-10-29 21:51:25 +0300

    Update AccountTest.java

--- src/ders2/AccountTest.java
+++ src/ders2/AccountTest.java
@@ -1,3 +1,4 @@
+package ders2;
 import java.util.Scanner;
 
 public class AccountTest {
@@ -9,6 +10,7 @@ public class AccountTest {
         String surname1 = "CAKMAK";
         int bakiye1 = -1;
 
+
         while (bakiye1 < 0) {
             System.out.println("1. kullanıcının bakiyesini girin (sıfırdan büyük olmalı): ");
             bakiye1 = scanner.nextInt();
@@ -16,13 +18,14 @@ public class AccountTest {
                 System.out.println("Hata: Lütfen sıfırdan büyük bir değer girin.");
             }
         }
-	    
+
         Account kisi1 = new Account(name1, surname1, bakiye1);
 
         String name2 = "HASAN";
         String surname2 = "HÜSEYİN";
         int bakiye2 = -1;
 
+        
         while (bakiye2 < 0) {
             System.out.println("2. kullanıcının bakiyesini girin (sıfırdan büyük olmalı): ");
             bakiye2 = scanner.nextInt();
@@ -30,13 +33,23 @@ public class AccountTest {
                 System.out.println("Hata: Lütfen sıfırdan büyük bir değer girin.");
             }
         }
-	    
+
         Account kisi2 = new Account(name2, surname2, bakiye2);
 
-        System.out.println("Kullanıcılar ");
+       
         System.out.println("1.Kullanıcı: " + kisi1.getName() + " ");
         System.out.println("1.Kullanıcı Soyadı: " + kisi1.getSurname() + " ");
         System.out.println("1.Kullanıcı Bakiye: " + kisi1.getBakiye() + " ");
+        
+        System.out.println("-----------------------------");
+        
+        System.out.println("2.Kullanıcı Adı: " + kisi2.getName() + " ");
+        System.out.println("2.Kullanıcı Soyadı: " + kisi2.getSurname() + " ");
+        System.out.println("2.Kullanıcı Bakiye: " + kisi2.getBakiye() + " ");
+        
+        System.out.println("-----------------------------");
+        
+        
 
         
         int money1 = -1;
@@ -47,24 +60,29 @@ public class AccountTest {
                 System.out.println("Hata: Lütfen sıfırdan büyük bir değer girin.");
             }
         }
-	    
-        kisi1.depositMoney(money1); 
-        
 
-       
+        kisi1.depositMoney(money1); 
+        System.out.println("1.Kullanıcıya Yüklenen Para: " + money1);
+        System.out.println("1.Kullanıcı Güncellenmiş Bakiye: " + kisi1.getBakiye()); 
+
+        
         int money2 = -1;
         while (money2 < 0) {
+        	System.out.println("-----------------------------");
             System.out.println("2.kullanıcıya yüklemek istediğiniz para (sıfırdan büyük olmalı): ");
             money2 = scanner.nextInt();
             if (money2 < 0) {
                 System.out.println("Hata: Lütfen sıfırdan büyük bir değer girin.");
             }
         }
-	    
-        kisi2.depositMoney(money2); 
 
-        System.out.println("2.Kullanıcı Adı: " + kisi2.getName() + " ");
-        System.out.println("2.Kullanıcı Soyadı: " + kisi2.getSurname() + " ");
-        System.out.println("2.Kullanıcı Bakiye: " + kisi2.getBakiye() + " "); 
+        kisi2.depositMoney(money2); 
+        System.out.println("2.Kullanıcıya Yüklenen Para: " + money2);
+        System.out.println("2.Kullanıcı Güncellenmiş Bakiye: " + kisi2.getBakiye()); 
+
+        
+        
     }
 }
+       
+     
